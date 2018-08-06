3fa10fbdeb1a81ff50e1c1f7f6de2c6018064dee
pramachandran <pramachandran@grubhub.com>
2018-08-05 21:16:29 -0400

    rotate right

---
+++ leetcode/rotate_right.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def _init_(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if head:
+            cur = head
+            count = 1
+            while cur.next:
+                count += 1
+                cur = cur.next
+            shift = count - (k % count)
+            if shift:
+                cur.next = head
+                idx = 0
+                while idx < shift:
+                    cur = cur.next
+                    idx += 1
+                tmp = cur.next
+                cur.next = None
+                return tmp
+            else:
+                return head
+        else:
+            return None
